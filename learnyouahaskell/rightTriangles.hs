89562caeb31914d169a1c93d56437d34220261c4
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-31 03:43:37 -0300

    fun with haskell

---
+++ learnyouahaskell/rightTriangles.hs
@@ -0,0 +1,10 @@
+-- Will produce [(6,8,10)]
+main :: IO ()
+main = do
+    let rightTriangles' = [ (a,b,c) | 
+                            c <- [1..10], 
+                            b <- [1..c], 
+                            a <- [1..b], 
+                            a^2 + b^2 == c^2, 
+                            a+b+c == 24]  
+    print rightTriangles'
