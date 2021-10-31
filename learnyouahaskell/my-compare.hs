9ca069c653d03c5757303ed8092f1c5f6875cdfe
diegopacheco <diego.pacheco.it@gmail.com>
2021-10-31 18:47:58 -0300

    more pattern matching

---
+++ learnyouahaskell/my-compare.hs
@@ -0,0 +1,10 @@
+-- myCompare
+-- compare 2 ORD and return GT, EQ or LT
+-- usage: myCompare 1 1
+-- usage: myCompare 1 2
+-- usage: myCompare 3 2
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT  
