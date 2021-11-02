f1514af5cef0bc60f2e0dff1bdd59a2c0b174106
diegopacheco <diego.pacheco.it@gmail.com>
2021-11-02 01:34:30 -0300

    monoids

--- learnyouahaskell/monoids-lists-are-monoids.hs
+++ learnyouahaskell/monoids-lists-are-monoids.hs
@@ -2,3 +2,9 @@
  main = do
      let x = [1,2,3] `mappend` [4,5,6]
      putStr(show x)  
+
+     let result = mconcat [[1,2],[3,6],[9]]
+     putStr(show result)  
+
+     let z = "pang" `mappend` mempty 
+     putStr(show z)  
