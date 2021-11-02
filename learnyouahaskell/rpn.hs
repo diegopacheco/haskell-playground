ba3bccc566e2e52bdb07acb1f64ad6d47aacc7ff
diegopacheco <diego.pacheco.it@gmail.com>
2021-11-02 00:02:11 -0300

    fun with RPN - polish notation calculator in Haskell

---
+++ learnyouahaskell/rpn.hs
@@ -0,0 +1,9 @@
+import Data.List  
+
+-- usage: solveRPN "10 4 6 + 2 + 2 * 1 -"
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN = head . foldl foldingFunction [] . words
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys
+            foldingFunction (x:y:ys) "+" = (x + y):ys
+            foldingFunction (x:y:ys) "-" = (y - x):ys
+            foldingFunction xs numberString = read numberString:xs
