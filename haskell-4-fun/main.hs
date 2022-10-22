eef56991a46475217f76338e7b3650fd47c605b6
diegopacheco <diego.pacheco.it@gmail.com>
2022-10-21 17:30:07 -0700

    haskell

--- haskell-4-fun/main.hs
+++ haskell-4-fun/main.hs
@@ -8,4 +8,12 @@ result3 = dropWhile (\x -> x<=3) [1,2,3,4,5,6,7,8,9]
 
 result4 = foldl (+) 0 [1,2,3]
 
-result5 = foldr (+) 0 [1..10] --
+result5 = foldr (+) 0 [1..10]
+
+result6 = show(4) ++ "2"
+
+result7 = head [1,2,3,4,5]
+
+result8 = tail [1,2,4,5,6]
+
+result9 = last [1,2,4,5,6]
