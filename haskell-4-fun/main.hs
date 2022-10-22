9b42919c209a52b04aafc51bad7f06a88fcb816c
diegopacheco <diego.pacheco.it@gmail.com>
2022-10-21 17:25:02 -0700

    haskell

--- haskell-4-fun/main.hs
+++ haskell-4-fun/main.hs
@@ -1,9 +1,11 @@
 addOne x = 1 + x
 
-x = map (\x -> 2+x) [1,2,3,4,5,6]
+result1 = map (\x -> 2+x) [1,2,3,4,5,6]
 
-y = filter (\x -> x>=2) (map addOne [1,2,3])
+result2 = filter (\x -> x>=2) (map addOne [1,2,3])
 
-z = dropWhile (\x -> x<=3) [1,2,3,4,5,6,7,8,9]
+result3 = dropWhile (\x -> x<=3) [1,2,3,4,5,6,7,8,9]
 
-w = foldl (+) 0 [1,2,3]
+result4 = foldl (+) 0 [1,2,3]
+
+result5 = foldr (+) 0 [1..10] --
