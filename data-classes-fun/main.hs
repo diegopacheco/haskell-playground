dc850ce0df46f4fcb75f7a9e174850492e053a9c
diegopacheco <diego.pacheco.it@gmail.com>
2022-10-21 18:41:53 -0700

    haskell

---
+++ data-classes-fun/main.hs
@@ -0,0 +1,5 @@
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)  
+data Point = Point Float Float deriving (Show)  
+
+c = Circle 10 20 5  
+p = Point 0.0 1.1
