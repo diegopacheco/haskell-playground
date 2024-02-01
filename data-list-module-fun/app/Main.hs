6ddb91b9b67f73ed587d5518314b3c2b4e01854c
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-01 00:18:27 -0800

    Haskell poc

--- data-list-module-fun/app/Main.hs
+++ data-list-module-fun/app/Main.hs
@@ -8,9 +8,17 @@ numUniques = length . nub
 getUnique :: (Eq a) => [a] -> [a]
 getUnique = nub
 
+join :: [Char]
+join = intercalate " " ["hey","there","guys"]
+
+cool :: [Char]
+cool = intersperse '.' "DRE"
+
 main :: IO ()
 main = do
     let nums = [1, 2, 2, 2, 3, 5, 1, 1, 1]
     let countUnique = numUniques nums
     print countUnique
     print (getUnique nums)
+    print cool
+    print join
