30a074c2b758443a0a7d307ca7599735e890220e
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-01 00:38:53 -0800

    Haskell poc

---
+++ data-map-module-fun/app/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+import qualified Data.List.NonEmpty as Map
+
+phoneBook :: [(String, String)]
+phoneBook = 
+    [("betty","555-2938")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ]
+
+findNumberByName :: (Eq k) => k -> [(k,v)] -> v
+findNumberByName key = snd . head . filter (\ (k, v) -> key == k)
+
+byTheList :: Map.NonEmpty (String, String)
+byTheList = Map.fromList [("betty","555-2938"),("bonnie","452-2928"),("lucille","205-2928")]
+
+main :: IO ()
+main = do
+    print phoneBook
+    putStrLn (findNumberByName "penny" phoneBook)
+    print byTheList
