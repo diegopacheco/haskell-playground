5bff1e52e645c02b5091c6c561a53a45d9d71ca8
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-01 00:24:57 -0800

    Haskell poc

--- data-char-module-fun/app/Main.hs
+++ data-char-module-fun/app/Main.hs
@@ -8,3 +8,4 @@ cool = all isAlphaNum "bobby283"
 main :: IO ()
 main = do
     print ("is this all alphanum?  " ++ show cool)
+    print ("is this all alphanum?  " ++ show (all isAlphaNum "bobby283@#"))
