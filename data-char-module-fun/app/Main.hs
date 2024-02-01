2c6693063048540045f821ce35a405042060d4f1
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-01 00:26:56 -0800

    Haskell poc

--- data-char-module-fun/app/Main.hs
+++ data-char-module-fun/app/Main.hs
@@ -8,8 +8,12 @@ cool = all isAlphaNum "bobby283"
 up :: Bool
 up = isUpper 'A'
 
+digit :: Bool
+digit = isDigit '9'
+
 main :: IO ()
 main = do
     print ("is this all alphanum?  " ++ show cool)
     print ("is this all alphanum?  " ++ show (all isAlphaNum "bobby283@#"))
     print ("is A UPPER ?  " ++ show up)
+    print ("is 9 digit ?  " ++ show digit)
