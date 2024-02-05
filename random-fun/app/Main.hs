7437818405d02bb587a21163ad6dc29d6945c6b0
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-05 00:06:31 -0800

    haskell poc

---
+++ random-fun/app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import System.Random
+import Data.List
+
+main :: IO()
+main = do
+    gen <- getStdGen
+    let randomChars = randomRs ('a','z') gen
+        (first20, rest) = splitAt 20 randomChars
+        (second20, _) = splitAt 20 rest
+    putStrLn first20
+    putStr second20
