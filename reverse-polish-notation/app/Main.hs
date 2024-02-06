a6b9a3f9fdcfe219696b7f7f87ba962bed03f139
diegopacheco <diego.pacheco.it@gmail.com>
2024-02-06 00:13:26 -0800

    haskell poc

---
+++ reverse-polish-notation/app/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN = head . foldl foldingFunction [] . words
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys
+            foldingFunction (x:y:ys) "+" = (x + y):ys
+            foldingFunction (x:y:ys) "-" = (y - x):ys
+            foldingFunction xs numberString = read numberString:xs
+
+main :: IO ()
+main = do 
+    putStrLn "RPN"
+    let result = solveRPN "2 3 +"
+    print result
